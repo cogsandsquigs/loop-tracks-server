f34fd39c12bdd8b1d3195e159b72d269eb80e653
Ian Pratt <ianjdpratt@gmail.com>
2022-07-21 08:24:35 -0500

    added ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: "loop-tracks-server",
+            script: "npm run start",
+            interpreter: "/home/ian/.nvm/versions/node/v18.6.0/bin/node",
+            repo: "https://github.com/cogsandsquigs/loop-tracks-server",
+        },
+    ],
+};
